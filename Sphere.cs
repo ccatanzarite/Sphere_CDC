f961eb5e7dc32141e41b37bebac4b51b4d1b8bab
Clayton <ccatanzarite@bellarmine.edu>
2020-01-15 10:40:50 -0500

    Added Sphere.cs and TestSphere.cs

---
+++ Sphere.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Sphere_CDC
+/**
+ * This class models a sphere object
+ * @author Clayton Catanzarite
+ * @version 1.0
+ */
+{
+    public class Sphere
+    {
+        private double diameter;
+        
+        public Sphere()
+        {
+            diameter = 2.0;
+        }
+
+        public Sphere(double diameter)
+        {
+            this.setDiameter(diameter);
+        }
+
+        public double getVolume()
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(diameter/2,3);
+        }
+
+        public double getSurfaceArea()
+        {
+            return 4.0 * Math.PI * Math.Pow(diameter / 2, 2);
+        }
+
+        public double getDiameter()
+        {
+            return diameter;
+        }
+
+        public void setDiameter(double diameter)
+        {
+            if (diameter < 2)
+                this.diameter = 2.0;
+            else
+                this.diameter = diameter;
+        }
+
+        public String toString()
+        {
+            return "Sphere [diameter=" + diameter + "]";
+        }
+    }
+}
---
+++ TestSphere.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sphere_CDC
+{
+    class TestSphere
+    {
+        public static void Main(String[] args)
+        {
+            Sphere ball = new Sphere();
+            Console.WriteLine("What is the diameter of the ball?");
+            ball.setDiameter(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("The balls diameter is: "+ball.getDiameter());
+            Console.WriteLine("The balls volume is: " + ball.getVolume());
+            Console.WriteLine("The balls Surface area is: " + ball.getSurfaceArea());
+        }
+    }
+}
